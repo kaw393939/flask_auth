1f28800973ff50305deb8f407aced457d147c929
kaw393939 <keith@webizly.com>
2022-03-09 13:01:44 -0500

    fixing some tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -14,13 +14,13 @@ def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Index" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"About Page" in response.data
+    assert b"About" in response.data
 
 def test_request_page1(client):
     """This makes the index page"""
