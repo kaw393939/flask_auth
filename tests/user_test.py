4616a20c4861600ccc59424ec8552b6601ae94f2
kaw393939 <keith@webizly.com>
2022-04-19 14:05:57 -0400

    updating test

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,14 +1,19 @@
+import logging
+
 from app import db
 from app.db.models import User, Song
 
 def test_adding_user(application):
+    log = logging.getLogger("myApp")
     with application.app_context():
+        #check that you don't have any users or songs
         assert db.session.query(User).count() == 0
         assert db.session.query(Song).count() == 0
         #showing how to add a record
         #create a record
         user = User('keith@webizly.com', 'testtest')
         #add it to get ready to be committed
+
         db.session.add(user)
         #call the commit
         db.session.commit()
@@ -19,9 +24,11 @@ def test_adding_user(application):
         #asserting that the user retrieved is correct
         assert user.email == 'keith@webizly.com'
         #this is how you get a related record ready for insert
-        user.songs= [Song("MyTitle","Joe Joe")]
+        user.songs= [Song("MyTitle","Joe Joe"),Song("Terrible Song","Joe Joe")]
         db.session.commit()
-        assert db.session.query(Song).count() == 1
+        for song in user.songs:
+            log.info(song.title)
+        assert db.session.query(Song).count() == 2
         song1 = Song.query.filter_by(title='MyTitle').first()
         assert song1.title == "MyTitle"
         #changing the title of the song
@@ -31,9 +38,9 @@ def test_adding_user(application):
         song2 = Song.query.filter_by(title='spam').first()
         assert song2.title == "spam"
         #checking cascade delete
-        #db.session.delete(user)
-        #assert db.session.query(User).count() == 0
-        #assert db.session.query(Song).count() == 0
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Song).count() == 0
 
 
 
